0a7eda8a01a1c6c8b1c42eaad1b089361dd96a7c
Craig Mulligan <craig@firingsuad.co.za>
2015-02-12 13:52:53 +0000

    first

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var request = require('request');
 var Lame = require('lame');
 var Speaker = require('speaker');
+var Firebase = require("firebase");
 
+var myFirebaseRef = new Firebase("https://valentines.firebaseio.com/");
 var text = 'Say hello to my little friend';
-
 var url = 'http://translate.google.com/translate_tts?tl=en&q=' + encodeURIComponent(text);
-request(url).pipe(new Lame.Decoder).pipe(new Speaker);
+ref.child("SEXCAVE").on("value", function(snap) {
+  if (snap.val() == "true") {
+    request(url).pipe(new Lame.Decoder).pipe(new Speaker);
+    console.log("true")
+  } else {
+  	console.log("false")
+  }
+})
+
+
+
+
+
+
